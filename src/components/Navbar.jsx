118714f6b6f3b4d9fa60fc59486f61449fe1c9f8
Abubakar Muhammad Magami <magamiabu@gmail.com>
2026-04-28 05:58:41 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,12 @@
+import { Link } from "react-router-dom";
 
+export default function Navbar() {
+  return (
+    <nav style={{ display: "flex", gap: "10px" }}>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/profiles">Profiles</Link>
+      <Link to="/search">Search</Link>
+      <Link to="/account">Account</Link>
+    </nav>
+  );
+}
